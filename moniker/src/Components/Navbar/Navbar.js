706e0f8cebf16a47956ec75faedbc613be3abd1a
Adam Reed <thirdeyeadam@gmail.com>
2021-06-30 15:06:02 -0500

    added placeholder header and footer components

---
+++ moniker/src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+
+
+class Navbar extends Component {
+    render() {
+      return (
+        
+        <div>
+        <h1 className="navbarTop">Header</h1>
+        </div>
+        
+      );
+    }
+  }
+  
+  export default Navbar;
